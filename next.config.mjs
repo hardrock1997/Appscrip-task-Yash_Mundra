0fe43e7ed2c784b757322796fb4f3a0684dc8827
vicfirth@1997 <yashmundra1997@gmail.com>
2025-06-22 12:33:29 +0530

    few changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{hostname:"fakestoreapi.com"}]
-    }
+        remotePatterns:[{hostname:"fakestoreapi.com"}],
+    },
+    output: "standalone",
 };
 
 export default nextConfig;
